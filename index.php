19b5b4b628666708e1c3a467874b00c4b2f656f7
DivaniNL <dylanvannierop@gmail.com>
2021-06-03 08:50:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+include "conn/conn.php";
+
+echo "test";
+
+
+?>
---
+++ register.php
@@ -0,0 +1,74 @@
+<?php
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+    <label for="username">Username</label>
+    <input id="username" type="text" name="username">
+
+    <label for="firstname">First Name</label>
+    <input id="firstname" type="text" name="firstname">
+
+    <label for="lastname">Last Name</label>
+    <input id="lastname" type="text" name="lastname">
+
+    <label for="email">Email-Adress</label>
+    <input id="email" type="email" name="email">
+
+    <label for="confirm_email">Confirm Email-Adress</label>
+    <input id="confirm_email" type="email" name="confirm_email">
+
+    <label for="password">Password</label>
+    <input id="password" type="password" name="password">
+
+    <label for="confirm_password">Confirm Password</label>
+    <input id="confirm_password" type="password" name="confirm_password">
+
+
+    <label for="driver">Favourite Driver</label>
+    <select name="favourite_driver" id="favourite_driver">
+        <option value="Lewis Hamilton">Lewis Hamilton</option>
+        <option value="Valtteri Bottas">Valtteri Bottas</option>
+        <option value="Max Verstappen">Max Verstappen</option>
+        <option value="Sergio Perez">Sergio Perez</option>
+        <option value="Lando Norris">Lando Norris</option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value="Charles Leclerc">Charles Leclerc</option>
+        <option value="Carlos Sainz">Carlos Sainz</option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+    </select>
+
+
+    <!-- Add avatar later -->
+
+
+    <input id="submit" type="submit" name="submit">
+
+
+
+    </form>
+</body>
+</html>
