c79db7a6aa737e8132b048c9a3468c40a2e59de9
DivaniNL <dylanvannierop@gmail.com>
2021-06-03 12:56:36 +0200

    logging and register backend complete

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
+
 <?php
 include "conn/conn.php";
-
-echo "test";
-
-
+if(!isset($_SESSION['logged-in'])){ //if login in session is not set
+    header("Location: login.php");
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>F1-Forum</title>
+</head>
+<body>
+    Dit Zie je als je ingelogd bent. Aan deze site wordt gewerkt.
+</body>
+</html>
